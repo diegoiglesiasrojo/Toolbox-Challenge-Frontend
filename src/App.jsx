9985c5640b0c931d3f3b22967f0d2ee287f0640e
Diego Iglesias <diegoiglesiasrojo@gmail.com>
2024-02-18 20:39:56 -0300

    v0.1.0 | First commit.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <main>
+      <h1>toolbox challenge frontend</h1>
+    </main>
+  );
+};
+
+export default App;
