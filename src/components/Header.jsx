c136c977afe5c4a3e96e041db21f14bb352363fe
Diego Iglesias <diegoiglesiasrojo@gmail.com>
2024-02-19 01:46:24 -0300

    v0.5.0 | Created Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { InputGroup, FormControl } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <header
+      style={{
+        width: "100%",
+        height: "20vh",
+        display: "flex",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        paddingTop: "5px",
+        paddingBottom: "5px",
+        backgroundColor: "#1B1A55",
+      }}
+    >
+      <h1 style={{ fontSize: "20px", color: "#9290C3", margin: "0px" }}>
+        Toolbox Challenge
+      </h1>
+      <InputGroup style={{ width: "130px" }}>
+        <FormControl
+          style={{
+            backgroundColor: "#535C91",
+            borderRadius: "10px",
+            borderWidth: "0px",
+          }}
+        />
+      </InputGroup>
+    </header>
+  );
+};
+
+export default Header;
